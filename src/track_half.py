863facd98f3445009236f4301356318f55dcfbbb
tangxiao <tangxiao@fmsh.com.cn>
2022-05-11 21:11:23 +0800

    🎉 feat: 新增精度测试功能

--- src/track_half.py
+++ src/track_half.py
@@ -145,7 +145,7 @@ def main(opt, data_root='/data/MOT16/train', det_root=None, seqs=('MOT16-05',), 
 
 
 if __name__ == '__main__':
-    torch.cuda.set_device(2)
+    torch.cuda.set_device(0)
     opt = opts().init()
 
     if not opt.val_mot16:
